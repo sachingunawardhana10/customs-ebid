e369c9d7a3f4412a4ba41752842ab4a77a0e0775
sachin <sachingunawardhana04@gmail.com>
2026-09-25 18:37:05 +0530

    chore(auth): initialize user auth service

---
+++ user-auth-service/src/test/java/lk/customsebid/userauth/UserAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.customsebid.userauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
